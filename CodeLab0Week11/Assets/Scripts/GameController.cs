0625a9dfdcbe9099e3d49cc121261671a10bcd94
zeeodd <evergreenskier@gmail.com>
2019-11-24 16:49:46 -0500

    First Text Box
    1. Initiated the general infra for the beginning sequence 2. Set up input system

---
+++ CodeLab0Week11/Assets/Scripts/GameController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject textBox;
+    public ZiyadController ziyad;
+
+    public bool secondText = false;
+    public bool readyForInput = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textBox.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ziyad.inPlace)
+        {
+            textBox.SetActive(true);
+            Invoke("SecondText", 1.5f);
+        }
+
+        if (secondText)
+        {
+            textBox.GetComponentInChildren<Text>().text = "Quick, use a spell!";
+        }
+    }
+
+    void SecondText()
+    {
+        secondText = true;
+    }
+}
---
+++ CodeLab0Week11/Assets/Scripts/ZiyadController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ZiyadController : MonoBehaviour
+{
+    RectTransform rt;
+    private Vector2 startPos = new Vector2(0f, -50f);
+    public bool inPlace;
+
+    void Awake()
+    {
+        rt = GetComponent<RectTransform>();
+        inPlace = false;
+    }
+
+    void Update()
+    {
+        if (!inPlace)
+        {
+            Invoke("PopUp", 1.0f);
+        }
+    }
+
+    void PopUp()
+    {
+        rt.anchoredPosition = Vector2.MoveTowards(rt.anchoredPosition, startPos, 5f);
+        if (rt.anchoredPosition == startPos)
+        {
+            inPlace = true;
+        }
+    }
+
+}
